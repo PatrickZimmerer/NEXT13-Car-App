e1e9d4a36d3308a53835d4b32521cc187841b083
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-07-31 11:02:46 +0200

    added function to fetch dynamic car images

--- utils/index.ts
+++ utils/index.ts
@@ -1,3 +1,5 @@
+import { CarProps } from '@/types';
+
 export async function fetchCars() {
 	const headers = {
 		'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY || '',
@@ -12,6 +14,21 @@ export async function fetchCars() {
 
 	return result;
 }
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+	const url = new URL('https://cdn.imagin.studio/getimage');
+
+	const { make, year, model } = car;
+
+	url.searchParams.append('customer', 'hrjavascript-mastery');
+	url.searchParams.append('make', make);
+	url.searchParams.append('modelFamily', model.split(' ')[0]);
+	url.searchParams.append('zoomType', 'fullscreen');
+	url.searchParams.append('modelYear', `${year}`);
+	url.searchParams.append('angle', `${angle}`);
+
+	return `${url}`;
+};
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
 	const basePricePerDay = 50; // Base rental price per day in dollars
