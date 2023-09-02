abb3b7a1be719f0bb7382e95dee3901f0d4a61ec
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-09-02 12:55:24 +0200

    change to client side rendering to avoid this error: https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbHZlR2ViVWZKUmxaNWM2Rl9TRWlqQTkyV3FtZ3xBQ3Jtc0trdjdIa25XWm1OYjFVeC01SGhKbzJxcE1IaXQ1TnNuMEVFc0Q4X0ZkajV6M18yWFFscGFYcV82OWVGT29OMmZyUDREa292NHpiajMxVFd4OXViRTBGckpDaEdMQ3AwVVQ1TVlGMWRhcXhUbllaMVNHRQ&q=https%3A%2F%2Fgithub.com%2Fvercel%2Fnext.js%2Fissues%2F49087&v=pUNSHPyVryU

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,48 @@
+'use client';
 import { CarCard, CustomFilter, Hero, SearchBar } from '@/components';
 import ShowMore from '@/components/ShowMore';
 import { fuels, yearsOfProduction } from '@/constants';
 import { FilterProps } from '@/types';
 import { fetchCars } from '@/utils';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
 
-export default async function Home({ searchParams }: { searchParams: FilterProps }) {
-	const allCars = await fetchCars({
-		manufacturer: searchParams.manufacturer || '',
-		year: searchParams.year || 2022,
-		fuel: searchParams.fuel || '',
-		limit: searchParams.limit || 10,
-		model: searchParams.model || '',
-	});
+export default function Home() {
+	const [allCars, setAllCars] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	// search states
+	const [manufacturer, setManufacturer] = useState('');
+	const [model, setModel] = useState('');
+
+	// filter states
+	const [fuel, setFuel] = useState('');
+	const [year, setYear] = useState(2022);
+
+	//pagination states
+	const [limit, setLimit] = useState(10);
+
+	const getCars = async () => {
+		setLoading(true);
+		try {
+			const result = await fetchCars({
+				manufacturer: manufacturer || '',
+				year: year || 2022,
+				fuel: fuel || '',
+				limit: limit || 10,
+				model: model || '',
+			});
+			setAllCars(result);
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setLoading(false);
+		}
+	};
+
+	useEffect(() => {
+		getCars();
+	}, [fuel, year, limit, manufacturer, model]);
 
 	const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
@@ -24,23 +55,31 @@ export default async function Home({ searchParams }: { searchParams: FilterProps
 					<p>Explore the cars you might like</p>
 				</div>
 				<div className="home__filters">
-					<SearchBar />
+					<SearchBar setManufacturer={setManufacturer} setModel={setModel} />
 					<div className="home__filter-container">
-						<CustomFilter title="fuel" options={fuels} />
-						<CustomFilter title="year" options={yearsOfProduction} />
+						<CustomFilter setFilter={setFuel} title="fuel" options={fuels} />
+						<CustomFilter setFilter={setYear} title="year" options={yearsOfProduction} />
 					</div>
 				</div>
-				{!isDataEmpty ? (
+				{allCars.length > 0 ? (
 					<section>
 						<div className="home__cars-wrapper">
 							{allCars?.map((car) => (
 								<CarCard car={car} />
 							))}
 						</div>
-						<ShowMore
-							pageNumber={(searchParams.limit || 10) / 10}
-							isNext={(searchParams.limit || 10) > allCars.length}
-						/>
+						{loading && (
+							<div>
+								<Image
+									src="/loader.svg"
+									alt="loader"
+									width={50}
+									height={50}
+									className="object-contain"
+								/>
+							</div>
+						)}
+						<ShowMore pageNumber={limit / 10} isNext={limit > allCars.length} />
 					</section>
 				) : (
 					<div className="home__error-container">
